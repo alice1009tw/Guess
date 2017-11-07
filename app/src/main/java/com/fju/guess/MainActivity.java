0308f979b5d7246adc7f1c5bad6d0423b1459d0a
alice1009tw <alice1009tw@gmail.com>
2017-11-07 12:20:27 +0800

    version1

---
+++ app/src/main/java/com/fju/guess/MainActivity.java
@@ -0,0 +1,39 @@
+package com.fju.guess;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationSet;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void guess(View view){
+        EditText edNumber = (EditText) findViewById(R.id.ed_number);
+        int Answer = Integer.parseInt(edNumber.getText().toString());
+        int Number;
+        Number = (int)(Math.random()*10)+1;
+        if(Answer < Number) {
+            Toast.makeText(this, "大一點", Toast.LENGTH_LONG).show();
+        }else if(Answer > Number){
+            Toast.makeText(this, "小一點", Toast.LENGTH_LONG).show();
+        }else{
+        new AlertDialog.Builder(this)
+                .setMessage("答對了")
+                .setPositiveButton("ok" , null)
+                .show();
+
+        }
+    }
+}
